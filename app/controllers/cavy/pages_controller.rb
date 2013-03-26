0cfcd33c3d0c9ad78ad6f3fa1a910302ea2d6b03
Thomas Muntaner <thomas.muntaner@gmail.com>
2013-03-26 01:20:21 +0100

    cleaned up some code

--- app/controllers/cavy/pages_controller.rb
+++ app/controllers/cavy/pages_controller.rb
@@ -3,56 +3,15 @@ require_dependency "cavy/application_controller"
 module Cavy
   class PagesController < ApplicationController
 
-    before_action :set_page, only: [:show, :update, :destroy]
+    before_action :set_page, only: [:page, :edit]
 
-    def page
-      @page = params[:title]? get_page(params[:title]) : Page.find_by(title: Cavy.root)
+    def page      
       render @page.render || 'cavy/pages/page'
     end
 
-    # GET /pages
-    def index
-      @pages = Page.all
-    end
-
-    # GET /pages/1
-    def show
-    end
-
-    # GET /pages/new
-    def new
-      @page = Page.new
-    end
-
-    # GET /pages/1/edit
     def edit
-      @page = params[:title]? get_page(params[:title]) : Page.find_by(title: Cavy.root)
       @edit = true
       render @page.render || 'cavy/pages/page'
-    end
-
-    # POST /pages
-    def create
-      @page = Page.new(page_params)
-
-      if @page.save
-        redirect_to @page, notice: 'Page was successfully created.'
-      else
-        render action: 'new'
-      end
-    end
-
-    # PATCH/PUT /pages/1
-    def update
-      @page = Page.find(params[:id])
-      @page.update(title: params[:content][:title][:value], content: params[:content][:content][:value])
-      render text: ""
-    end
-
-    # DELETE /pages/1
-    def destroy
-      @page.destroy
-      redirect_to pages_url, notice: 'Page was successfully destroyed.'
     end
 
     private
@@ -61,18 +20,13 @@ module Cavy
         raise ActionController::RoutingError.new('Not Found')
       end
 
+      def set_page
+        @page = params[:title]? get_page(params[:title]) : Page.find_by(title: Cavy.root)
+      end
+
       def get_page(title)
         Page.find_by(title: title) || not_found
       end
 
-      # Use callbacks to share common setup or constraints between actions.
-      def set_page
-        @page = Page.find(params[:id])
-      end
-
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def page_params
-        params.require(:page).permit(:content, :title)
-      end
   end
 end
