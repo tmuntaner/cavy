d09023f9a8b2c0b9e933fc1152d1b21500e32de9
Thomas Muntaner <thomas.muntaner@gmail.com>
2013-03-28 11:34:11 +0100

    testing guest permissions

--- spec/models/cavy/permissions/guest_permission_spec.rb
+++ spec/models/cavy/permissions/guest_permission_spec.rb
@@ -4,95 +4,52 @@ module Cavy
   describe Permissions::GuestPermission do
     subject { Permissions.permission_for(nil) }
 
-    # it "allows investigations" do
-    #   should_not allow(:investigations, :new_note)
-    #   should_not allow(:investigations, :create_note)
-    #   should_not allow(:investigations, :edit)
-    #   should_not allow(:investigations, :update)
-    #   should_not allow(:investigations, :destroy)
-    #   should_not allow(:investigations, :show)
-    #   should_not allow(:investigations, :close_case)
-    #   should_not allow(:investigations, :open_case)
-    #   should_not allow(:investigations, :index)
-    #   should_not allow(:investigations, :create)
-    # end
+    it "allows admin pages" do
+      should_not allow('cavy/admin_pages', :index)
+      should_not allow('cavy/admin_pages', :show)
+      should_not allow('cavy/admin_pages', :update)
+      should_not allow('cavy/admin_pages', :edit)
 
-    # it "allows requests" do
+      should_not allow('cavy/admin_pages', :new)
+      should_not allow('cavy/admin_pages', :create)
+      should_not allow('cavy/admin_pages', :delete)
+    end
 
-    #   should allow(:requests, :index)
-    #   should allow(:requests, :show)
-    #   should_not allow(:requests, :new)
-    #   should_not allow(:requests, :create)
-    #   should_not allow(:requests, :edit)
-    #   should_not allow(:requests, :update)
-    # end
+    it "allows to view pages" do
+      should allow('cavy/pages', :page)
+      should_not allow('cavy/pages', :edit)
+      should_not allow('cavy/pages', :update)
+    end
 
-    # it "allows sightings" do
-    #   should allow(:sightings, :index)
-    #   should allow(:sightings, :show)
-    #   should_not allow(:sightings, :new)
-    #   should_not allow(:sightings, :create)
-    #   should_not allow(:sightings, :edit)
-    #   should_not allow(:sightings, :update)
-    #   should_not allow(:sightings, :add_to_investigation)
-    #   should_not allow(:sightings, :add_sighting)
-    # end
+    it "should_not allow sessions" do
+      should allow('cavy/sessions', :new)
+      should allow('cavy/sessions', :create)
+      should allow('cavy/sessions', :destroy)
+    end
 
-    # it "allows pages" do
-    #   should allow(:pages, :welcome)
-    #   should allow(:pages, :map)
-    #   should allow(:pages, :location)
-    #   should allow(:pages, :compendium)
-    #   should allow(:pages, :contact)
-    # end
+    it "should_not allow admin panel" do
+      should_not allow('cavy/admin', :overview)
+      should_not allow('cavy/admin', :users)
+      should_not allow('cavy/admin', :new_user)
+    end
 
-    # it "allows hunters" do
-    #   should allow(:hunter, :show)
-    #   should allow(:hunter, :index)
-    #   should_not allow(:hunter, :become_hunter)
-    #   should_not allow(:hunter, :update_to_hunter)
-    # end
+    it "should_not allow mercury" do
+      should_not allow('cavy/mercury', :edit)
+      should_not allow('cavy/mercury', :resource)
+      should_not allow('cavy/mercury', :snippet_options)
+      should_not allow('cavy/mercury', :snippet_preview)
+      should_not allow('cavy/mercury', :test_page)
+      should_not allow('cavy/mercury', :image)
+    end
 
-    # it "allows creatures" do
-    #   should allow(:creatures, :index)
-    #   should allow(:creatures, :show)
-    #   should_not allow(:creatures, :new)
-    #   should_not allow(:creatures, :edit)
-    #   should_not allow(:creatures, :update)
-    #   should_not allow(:creatures, :create)
-    # end
+    it "should_not allow users" do
+      should_not allow('cavy/users', :index)
+      should_not allow('cavy/users', :show)
+      should_not allow('cavy/users', :new)
+      should_not allow('cavy/users', :edit)
+      should_not allow('cavy/users', :create)
+      should_not allow('cavy/users', :update)
+    end
 
-    # it "allows sessions" do
-    #   should allow(:sessions, :new)
-    #   should allow(:sessions, :create)
-    #   should allow(:sessions, :destroy)
-    # end
-
-    # it "allows users" do
-    #   should allow(:users, :new)
-    #   should allow(:users, :create)
-    #   should allow(:users, :try)
-    #   should_not allow(:users, :edit)
-    #   should_not allow(:users, :update)
-    # end
-
-    # it "allows contact form" do
-    #   should allow(:contacts,:new)
-    #   should allow(:contacts,:create)
-    #   should_not allow(:contacts,:edit)
-    #   should_not allow(:contacts,:update)
-    #   should_not allow(:contacts,:destroy)
-    #   should_not allow(:contacts,:index)
-    #   should_not allow(:contacts,:show)
-    # end
-    # it "allows feature requests" do
-    #   should_not allow(:features,:new)
-    #   should_not allow(:features,:create)
-    #   should_not allow(:features,:edit)
-    #   should_not allow(:features,:update)
-    #   should_not allow(:features,:destroy)
-    #   should allow(:features,:index)
-    #   should allow(:features,:show)
-    # end
   end
 end
