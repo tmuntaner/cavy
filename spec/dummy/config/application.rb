112fad042646a93db650f60b93f89689f054c123
Thomas Muntaner <thomas.muntaner@gmail.com>
2013-04-14 11:46:12 +0200

    precompiling mercury.css separately

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -26,7 +26,7 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += ["cavy/mercury_loader.js", "cavy/mercury_save.js", "cavy/mercury.js"]
+    config.assets.precompile += ["cavy/mercury.css", "cavy/mercury_loader.js", "cavy/mercury_save.js", "cavy/mercury.js"]
   end
 end
 
