8f42f62130e8c5b75b0b10bdb931e0939092e3fa
Thomas Muntaner <thomas.muntaner@gmail.com>
2013-05-22 17:46:44 +0200

    added route helper

--- lib/cavy/railtie.rb
+++ lib/cavy/railtie.rb
@@ -1,9 +1,11 @@
 require_relative 'helpers/view_helpers'
+require_relative 'helpers/link_helpers'
 
 module Cavy
   class Railtie < Rails::Railtie
     initializer 'cavy.view_helpers' do
       ActionView::Base.send :include, ViewHelpers
+      ActionView::Base.send :include, LinkHelpers
     end
   end
 end
