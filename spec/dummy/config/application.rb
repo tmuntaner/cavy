7689f085496ad147a5aef27821c1648cddf9b431
Thomas Muntaner <thomas.muntaner@gmail.com>
2016-07-07 16:45:14 +0200

    Got the application working with the Rubymine IDE
    * updated the spec application
    * needed to require rails in the Gemfile, the gemspec was not enough

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,22 +1,22 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-# Pick the frameworks you want:
 require 'rails/all'
-# require 'krypt/ossl'
-#require "active_record/railtie"
-#require "action_controller/railtie"
-#require "action_mailer/railtie"
-#require "sprockets/railtie"
-require "jquery-rails"
-require "carrierwave"
-#require "turbolinks"
+require 'jquery-rails'
+require 'carrierwave'
 
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "cavy"
 
 module Dummy
   class Application < Rails::Application
-    config.assets.precompile += ["cavy/mercury.css", "cavy/mercury_loader.js", "cavy/mercury_save.js", "cavy/mercury.js"]
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.precompile += %w(cavy/mercury.css cavy/mercury_loader.js cavy/mercury_save.js cavy/mercury.js)
     I18n.enforce_available_locales = false
+
   end
 end
