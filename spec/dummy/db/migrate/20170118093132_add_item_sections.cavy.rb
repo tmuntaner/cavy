b52601afabcd58bba8b6a76e266686b8b13618e6
Thomas Muntaner <thomas.muntaner@gmail.com>
2017-01-28 23:04:05 +0100

    Removed migrations from dummy

--- spec/dummy/db/migrate/20170118093132_add_item_sections.cavy.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from cavy (originally 20141116193508)
-class AddItemSections < ActiveRecord::Migration
-  def change
-    create_table :cavy_item_sections do |t|
-      t.string  :title
-      t.integer :item_groups, array: true
-    end
-  end
-end
